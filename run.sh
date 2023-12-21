fe4a81ac42c4dacc5da8c4e8e2a51ab8c8d37075
picasso09 <119201284+picasso09@users.noreply.github.com>
2023-12-22 01:08:54 +0700

    RzaIX X picasso09

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 git submodule init
 git submodule update
-export KBUILD_BUILD_USER="picasso"
+export KBUILD_BUILD_USER="RzaIX"
+export KBUILD_BUILD_HOST="picasso09"
 export PATH="$(pwd)/clang/bin:$PATH"
     make -j$(nproc --all) O=out ARCH=arm64 certus_defconfig
     make -j$(nproc --all) ARCH=arm64 O=out \
