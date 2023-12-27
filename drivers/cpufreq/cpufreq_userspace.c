0a175a19a3cc304ef2d3aca910739b80fc12ee29
picasso09 <sayazaki282901@gmail.com>
2023-12-27 10:26:03 +0000

    certus arm

--- drivers/cpufreq/cpufreq_userspace.c
+++ drivers/cpufreq/cpufreq_userspace.c
@@ -121,7 +121,7 @@ static void cpufreq_userspace_policy_limits(struct cpufreq_policy *policy)
 #ifndef CONFIG_CPU_FREQ_DEFAULT_GOV_USERSPACE
 static
 #endif
-static struct cpufreq_governor cpufreq_gov_userspace = {
+struct cpufreq_governor cpufreq_gov_userspace = {
 	.name		= "userspace",
 	.init		= cpufreq_userspace_policy_init,
 	.exit		= cpufreq_userspace_policy_exit,
--- run.sh
+++ run.sh
@@ -2,9 +2,8 @@ git submodule init
 git submodule update
 export KBUILD_BUILD_USER="RzaIX"
 export KBUILD_BUILD_HOST="picasso09"
-export PATH="$(pwd)/pepek/bin:$PATH"
-    make -j$(nproc --all) O=out ARCH=arm64 certus_defconfig
-    make -j$(nproc --all) ARCH=arm64 O=out \
-                          CC=clang \
-                          CROSS_COMPILE=aarch64-linux-gnu- \
+# export PATH="$(pwd)/pepek/bin:$PATH"
+    make -j$(nproc --all) O=out ARCH=arm certusarm_defconfig
+    make -j$(nproc --all) ARCH=arm O=out \
+                          CROSS_COMPILE=arm-linux-gnueabi- \
                           CROSS_COMPILE_ARM32=arm-linux-gnueabi-
