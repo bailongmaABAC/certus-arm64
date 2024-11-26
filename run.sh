5a9b08b1c9bddb68f0536caa1760125ac640e1f2
bailongmaABAC <134177650+bailongmaABAC@users.noreply.github.com>
2024-11-27 00:25:07 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@ git clone https://github.com/picasso09/clang-9.0.3-r353983c1.git clang
 export KBUILD_BUILD_USER="RzaIX"
 export KBUILD_BUILD_HOST="picasso09"
 export PATH="$(pwd)/clang/bin:$PATH"
+clang -v
     make -j$(nproc --all) O=out ARCH=arm64 certus_defconfig
     make -j$(nproc --all) ARCH=arm64 O=out \
                           CC=clang \
