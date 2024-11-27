82b2c97e8ddbdff4bbc4b8edaa45faf3a2b6b8d9
bailongmaABAC <134177650+bailongmaABAC@users.noreply.github.com>
2024-11-27 12:40:23 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
-git clone https://github.com/picasso09/clang-9.0.3-r353983c1.git clang
+#git clone https://github.com/picasso09/clang-9.0.3-r353983c1.git clang
+clang-version=r346389c aria2c -o clang.tar.gz https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/android10-release/clang-${clang-version}.tar.gz
+tar -C ./clang -zxf clang.tar.gz
 export KBUILD_BUILD_USER="RzaIX"
 export KBUILD_BUILD_HOST="picasso09"
 export PATH="$(pwd)/clang/bin:$PATH"
